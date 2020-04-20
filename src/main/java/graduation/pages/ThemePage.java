5bcda66f533ee4197336b1ad737ac516b905a6a7
Ttorf <41079692+Ttorf@users.noreply.github.com>
2020-04-20 16:42:41 +0300

    fix test with Response

--- src/main/java/graduation/pages/ThemePage.java
+++ src/main/java/graduation/pages/ThemePage.java
@@ -32,7 +32,7 @@ public class ThemePage {
         this.webDriver = webDriver;
     }
 
-    private boolean isElemenExist(By by) {
+    private boolean isElementExist(By by) {
         try {
             webDriver.findElement(by);
             return true;
@@ -183,7 +183,7 @@ public class ThemePage {
         WebElement element = webDriver.findElement(buttonSubmit);
         element.click();
 
-        if (isElemenExist(alertYouCantSendAnswersSoFastly)) {
+        if (isElementExist(alertYouCantSendAnswersSoFastly)) {
             if (webDriverWaitVisibility(alertYouCantSendAnswersSoFastly).isDisplayed()) {
                 Thread.sleep(1500);
                 element.click();
